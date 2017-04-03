cfb901450fd52edb84e4940a8f5cf89286ed0b71
Jakob <jakob>
2017-04-03 10:13:06 +0200

    Add database script

---
+++ database.sql
@@ -0,0 +1,33 @@
+DROP TABLE orderitems;
+DROP TABLE orders;
+DROP TABLE materials;
+DROP TABLE users;
+
+CREATE TABLE users (
+	username VARCHAR(100) PRIMARY KEY NOT NULL,
+	password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE materials (
+	id INTEGER PRIMARY KEY NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	type VARCHAR(100) NOT NULL,
+	size DECIMAL(5, 3) NOT NULL
+);
+
+CREATE TABLE orders (
+	id INTEGER PRIMARY KEY NOT NULL,
+	customer_name VARCHAR(100) NOT NULL,
+	customer_email VARCHAR(100) NOT NULL,
+	customer_phone VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE orderitems (
+	id INTEGER PRIMARY KEY NOT NULL,
+	order_id INTEGER NOT NULL REFERENCES orders(id),
+	material_id INTEGER NOT NULL REFERENCES materials(id),
+	quantity INTEGER NOT NULL
+);
+
+
+
