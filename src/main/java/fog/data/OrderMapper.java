9026e31f030e66c15a38973f37670f4e96455828
Pravien.T <Pravien95@gmail.com>
2017-04-03 10:43:34 +0200

    Merge origin/master
    Conflicts:
    	SemesterAfslutningWebApp/test/DAOtest.java

---
+++ src/main/java/fog/data/OrderMapper.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fog.data;
+
+import fog.domain.Order;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Pravien
+ */
+public class OrderMapper
+{
+    
+    private final Connector connector;
+
+    public OrderMapper(Connector connector) {
+        this.connector = connector;
+       
+    }
+    
+    
+    public Order getOrderById(int id) throws SQLException {
+        
+        try {
+            String query = "SELECT * FROM orders  WHERE id = ?;";
+            PreparedStatement stmt = connector.getConnection().prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet res = stmt.executeQuery();
+            
+            if (res.next()){
+               int orderId = res.getInt("id");
+               String customerName = res.getString("customer_name");
+               String customerEmail = res.getString("customer_email");
+               String customerPhone = res.getString("customer_phone");
+               Order newOrder = new Order(orderId,customerName,customerEmail,customerPhone);
+               
+               return newOrder;
+               
+            }
+            else 
+                return null;
+            
+        }
+        
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        
+    }
+    
+    public void deleteOrderById(int id) throws SQLException{
+     
+        
+            String query = "DELETE FROM orders where id = ?";
+            PreparedStatement stmt = connector.getConnection().prepareStatement(query);
+            stmt.setInt(1,id);
+            stmt.executeUpdate();  
+    }
+}
---
+++ src/main/java/fog/domain/Order.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fog.domain;
+
+/**
+ *
+ * @author Pravien
+ */
+public class Order
+{
+    private int id;
+    private String customerName,customerMail,customerPhone;
+
+    public Order(int id, String customerName, String customerMail, String customerPhone)
+    {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerMail = customerMail;
+        this.customerPhone = customerPhone;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName)
+    {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerMail()
+    {
+        return customerMail;
+    }
+
+    public void setCustomerMail(String customerMail)
+    {
+        this.customerMail = customerMail;
+    }
+
+    public String getCustomerPhone()
+    {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone)
+    {
+        this.customerPhone = customerPhone;
+    } 
+}
