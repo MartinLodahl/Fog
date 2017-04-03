566819fea4a6d6bd07960bb6035cb75fd2b7bdd4
Martin <secretagent1@cia.com>
2017-04-03 10:15:20 +0200

    connector

--- src/main/java/fog/data/Connector.java
+++ src/main/java/fog/data/Connector.java
@@ -5,10 +5,43 @@
  */
 package fog.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author MartinLodahl
  */
 public class Connector {
-    
+      private Connection con;
+    private static Connector instance;
+    private static PreparedStatement stmt;
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost:3306/test";
+    private static String id = "root";
+    private static String pw = "fuck";
+
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(URL, id, pw);  // The connection will be released upon program 
+
+        } catch (Exception e) {
+            System.out.println("\n*** Remember to insert your  ID and PW in the DBConnector class! ***\n");
+            System.out.println("error in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+
+        return con;
+    }
+
+    public void releaseConnection(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 }
