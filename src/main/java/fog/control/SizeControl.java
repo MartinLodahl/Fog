a721a811f0f49d5ccb6cea819fac875a5edf0e4b
Martin <cphma368@cphbusiness.dk>
2017-05-04 12:08:39 +0200

    StringBuilder

--- src/main/java/fog/control/SizeControl.java
+++ src/main/java/fog/control/SizeControl.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "SizeControl", urlPatterns = {"/size"})
 public class SizeControl extends HttpServlet {
 
@@ -44,16 +43,33 @@ public class SizeControl extends HttpServlet {
             boolean build = request.getParameter("build") != null;
             String callDate = request.getParameter("callDate");
             System.out.println(callDate);
+            /*
+mm/dd/yyyy
+måned/dag/år
+dd-mm-yyyy
+dag-måned-år
 
-            Order order = new Order(0, name, email, phone, false, width, length, height, skur, build, false,callDate);
+             */
+            
+            String tempDay = callDate.substring(3, 5);
+            String tempMonth = callDate.substring(0,2);
+            String tempYear = callDate.substring(6);
+            
+            StringBuilder SBcallDate = new StringBuilder();
+            
+            SBcallDate.append(tempDay + "-" + tempMonth + "-" + tempYear);
+            callDate = SBcallDate.toString();
+            System.out.println(callDate);
+
+            Order order = new Order(0, name, email, phone, false, width, length, height, skur, build, false, callDate);
             FacadeMapper fm = new FacadeMapper();
             System.out.println("create order");
             int orderID = fm.createOrder(order);
-             System.out.println("end create order");
-            
+            System.out.println("end create order");
+
             ArrayList<Material> list = fm.CreateMaterialList(length, width, skur, height);
             fm.CreateOrderItems(list, orderID);
-            
+
             //Sends the materiallist to the frontend
             ArrayList<OrderItem> orderItems = fm.getOrderItems(orderID);
             request.setAttribute("orderItems", orderItems);
