0740265bdb6ff3f27150e8624574f361462554ad
Jakob <jakob>
2017-05-10 12:19:11 +0200

    Clean up javascript

--- src/main/webapp/disableDates.js
+++ src/main/webapp/disableDates.js
@@ -1,37 +1,25 @@
-var disableddates = ["05-05-2017"];
+var disableddates = [];
 //Jquery .val til at få values ud af inputs fra JSP.
-var i = ($('input[id="i"]').val());
+var i = ($('#i').val());
 //loop igennem nogle forskellige inputs (hidden), vi har navngivet efter deres placering i tidligere ArrayList.
 for(var j=0; j<i; j++){
-     disableddates.push($('input[id="date'+j+'"]').val());
+     disableddates.push($('#date'+j).val());
  } 
 
-
-function DisableSpecificDates(date) {
-  
+function disableSpecificDates(date) {
     var string = jQuery.datepicker.formatDate('dd-mm-yy', date);
     return [disableddates.indexOf(string) === -1];
   }
   
-  
-
-
-var MyDate = new Date();
-var MyDateString;
-
-MyDate.setDate(MyDate.getDate() + 20);
-
-MyDateString = ('0' + (MyDate.getMonth()+1)).slice(-2) + '/'
-             + ('0' + MyDate.getDate()).slice(-2)  + '/'
-             + MyDate.getFullYear();
-
-document.getElementById('callDate').value=MyDateString;
-
-
-
+var date = new Date();
+date.setDate(date.getDate() + 20);
+var dateString = ('0' + (date.getMonth()+1)).slice(-2) + '/'
+             + ('0' + date.getDate()).slice(-2)  + '/'
+             + date.getFullYear();
+document.getElementById('callDate').value = dateString;
 
 $(function() {
   $("#callDate").datepicker({
-    beforeShowDay: DisableSpecificDates
+    beforeShowDay: disableSpecificDates
   });
 });
